9281d71a1926f71ed51fff1674a7114fafbe5f4f
Shelby Jorgensen <jorgensen.p.shelby@gmail.com>
2023-03-30 18:34:20 -0700

    first commit

---
+++ CS380_Lab0.java
@@ -0,0 +1,10 @@
+package packagetest;
+
+public class CS380_Lab0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Lab CS 380");
+	}
+
+}
